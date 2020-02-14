76b4cf1e33ab2bd9b56f0ec79c7b8857784affde
Aobakwe Mokalake <aobakwe.mokalake@momentummetropolitan.co.za>
2020-02-14 10:31:59 +0200

    just new buld
    Signed-off-by: Aobakwe Mokalake <aobakwe.mokalake@momentummetropolitan.co.za>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Starting of the function
+// Just for control 
 function App() {
   return (
     <div className="App">
